daa2e18dae08f497376eb015439c065e49a10a2e
IdanHalf <smexyidan@gmail.com>
2021-04-01 12:16:49 +0300

    vaccination page, changing styles

--- IndexHamburger.js
+++ IndexHamburger.js
@@ -6,3 +6,12 @@ hamburger.addEventListener('click',()=>{
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
 })
+
+document.addEventListener('scroll',()=>{
+    var scroll_position = window.scrollY;
+    if(scroll_position > 250){
+        header.style.backgroundImage = "linear-gradient(60deg,#29323c 0%,#485563 100%)"
+    }else{
+        header.style.backgroundImage = "linear-gradient(transparent,transparent)"
+    }
+})
