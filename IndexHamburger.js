b9d7ee1d8b6fd4063997be544f105da210b5b0ec
IdanHalf <smexyidan@gmail.com>
2021-04-22 13:59:41 +0300

    Update IndexHamburger.js
    fixed the bug which hamburger didnt close after clicking on one of the items on the list

--- IndexHamburger.js
+++ IndexHamburger.js
@@ -1,12 +1,15 @@
 const hamburger = document.querySelector('.header .nav-bar .nav-list .list');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a'); // querySelectorAll because there is more than 1 item (2 lists)
 const header = document.querySelector('.header.container');
 
+// toggle the hamburger click
 hamburger.addEventListener('click',()=>{
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
 })
 
+// header effect, when the scroll position is above 250, the background is strong, else is it transparent.
 document.addEventListener('scroll',()=>{
     var scroll_position = window.scrollY;
     if(scroll_position > 250){
@@ -15,3 +18,11 @@ document.addEventListener('scroll',()=>{
         header.style.backgroundImage = "linear-gradient(transparent,transparent)"
     }
 })
+
+// toggle every item in the list to close it if it is clicked after hamburger list is opened.
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
